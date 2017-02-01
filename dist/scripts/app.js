42d90ef0767365d6763171114bfc69a6dceb0cb4
Brandon Caples <Brandon Caples>
2017-01-31 21:13:42 -0600

    Checkpoint 5 corrections

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -9,6 +9,7 @@
         $stateProvider
             .state('landing', {
                 url: '/',
+                controller: 'LandingCtrl as landing',
                 templateUrl: '/templates/landing.html'
             })
             .state('album', {
@@ -17,11 +18,12 @@
             })
             .state('collection', {
                 url: '/collection',
+                controller: 'CollectionCtrl as collection',
                 templateUrl: '/templates/collection.html'
             });
     }
     
     angular
-        .module('blocJams', ['ui.router']);
+        .module('blocJams', ['ui.router'])
         .config(config);
 })();
