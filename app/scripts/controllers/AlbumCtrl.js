f04e7611d130efd183d52bf9d7ce973c03f604c6
Brandon Caples <Brandon Caples>
2017-02-01 20:51:35 -0600

    Finished Assignment 5 ... finally!

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,10 @@
 (function() {
     function AlbumCtrl() {
         this.albumData = [];
-        this.albumData.push(angular.copy(albumPicasso));
+        this.albumData.push(angular.copy(albumPicasso)).songs;
     }
     
     angular
-        .module('blocJams', [])
+        .module('blocJams')
         .controller('AlbumCtrl', AlbumCtrl);
 })();
